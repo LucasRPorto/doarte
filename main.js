f73dd8b2a1d789748d1086faeb0f5858ebbd1929
LucasRPorto <lucasrodriguesporto@gmail.com>
2024-01-08 19:06:03 -0300

    feat: (implemented-logic-post-form) - Lucas Porto

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const urlApi = "http://localhost:8080/doador"
-const btnDoacao = document.querySelectorAll('.kit__botao')
-const btnCadastroDoador = document.querySelector('.cadastrar-form')
+const urlApi = "http://localhost:8080/doador";
+const btnDoacao = document.querySelectorAll('.kit__botao');
+const btnCadastroDoador = document.querySelector('.cadastrar-form');
+const formularioCadastro = document.querySelector('.formularioCadastro');
 
 btnDoacao.forEach((btnDoado) => {
     btnDoado.addEventListener('click', (evento)=> {
@@ -17,37 +18,62 @@ async function buscaDoadores(){
 }
 // Testando GET da API doarte
 
-async function cadastraDoador(doador){
-    const retornoPost = await fetch('http://localhost:8080/doador', {
-    method: "POST",
-    body: JSON.stringify(doador),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
+async function cadastraDoador(doador) {
+    try {
+        const resposta = await fetch('http://localhost:8080/doador', {
+            method: "POST",
+            body: JSON.stringify(doador),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        const codigoResposta = resposta.status;
+
+        console.log(`Código de resposta: ${codigoResposta}`);
+
+        if (resposta.ok) {
+            console.log('Dados recebidos:', doador);
+        } else {
+            console.error('Erro na requisição:', resposta.statusText);
         }
-    })
-    const retornoJson = await retornoPost.json();
-    //await console.log(retornoJson);
+    } catch (erro) {
+        console.error('Erro na requisição:', erro.message);
+    }
 }
 
-const doador = {
-    "nome": "Lucas Teste",
-    "email": "lucasteste@hotmail.com",
-    "telefone": "62986390017",
-    "nascimento": "1902-08-06",
-	"endereco":{
-		"cep": "74610320",
-    	"rua": "Dom Jao",
-    	"bairro": "Bairro De Teste",
-   	 	"cidade": "Terezina",
-    	"uf": "PI",
-    	"numero": "32",
-    	"complemento": "ap 100"
-	} 
-}
 
-//cadastraDoador(doador); // COMENTADO POIS O MÉTODO ESTÁ REALIZANDO POST PARA A API E ENCHENDO O BD
+formularioCadastro.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-console.log("LISTANDO DOADORES APÓS A REQUISIÇÃO POST (5s de Delay): ");
-function funcaoDeExemplo() {
-    buscaDoadores();
-  } setTimeout(funcaoDeExemplo, 5000);
+    const form = document.querySelector('.formularioCadastro');
+
+    const nome = form.elements['nome'].value;
+    const email = form.elements['email'].value;
+    const telefone = form.elements['telefone'].value;
+    const nascimento = form.elements['dataNascimento'].value;
+    const cep = form.elements['cep'].value;
+    const rua = form.elements['rua'].value;
+    const bairro = form.elements['bairro'].value;
+    const cidade = form.elements['cidade'].value;
+    const uf = form.elements['uf'].value;
+    const numero = form.elements['numero'].value;
+    const complemento = form.elements['complemento'].value;
+
+    const doador = {
+        "nome": nome,
+        "email": email,
+        "telefone": telefone,
+        "nascimento": nascimento,
+        "endereco": {
+            "cep": cep,
+            "rua": rua,
+            "bairro": bairro,
+            "cidade": cidade,
+            "uf": uf,
+            "numero": numero,
+            "complemento": complemento
+        }
+    }
+    cadastraDoador(doador);
+    formularioCadastro.reset();
+});
